db4254423070d70b860c3bbccb840c7ff25dbec9
Kieran Mace <kieran.mace@gmail.com>
2016-11-04 16:50:41 -0700

    created an apply that does the comparrison for all conditions that have both drug and no drug for WT

--- src/sci/compare_drug_to_no_drug.R
+++ src/sci/compare_drug_to_no_drug.R
@@ -2,43 +2,62 @@ source('../utils/load_libraries.R')
 source('../utils/load_functions.R')
 source('../utils/load_data.R')
 
+# only look at WT Cells
+wt = rlog_meta$Strain == 'WT'
+rlog = rlog[ ,wt]
+rlog_meta = rlog_meta[wt, ]
+
+# only look at YPD (we only have no drug control for YPD)
+ypd = rlog_meta$Media == 'YPD'
+rlog = rlog[ ,ypd]
+rlog_meta = rlog_meta[ypd, ]
+
+get_sample_mean = function(x) apply(x,1,mean)
+# Establish Control sets, these will be denominators throughout.
 control_noDrug = which(
     rlog_meta$Stress == 'None' &
-    rlog_meta$Drug == 'None' &
-    rlog_meta$Media == 'YPD' &
-    rlog_meta$Strain == 'WT')
-
-condition_noDrug = which(
-    rlog_meta$Stress == 'Heatshock' &
-    rlog_meta$Drug == 'None' &
-    rlog_meta$Media == 'YPD' &
-    rlog_meta$Strain == 'WT')
+    rlog_meta$Drug == 'None')
 
 control_Drug = which(
     rlog_meta$Stress == 'None' &
-    rlog_meta$Drug == 'Cocktail' &
-    rlog_meta$Media == 'YPD' &
-    rlog_meta$Strain == 'WT')
-
-condition_Drug = which(
-    rlog_meta$Stress == 'Heatshock' &
-    rlog_meta$Drug == 'Cocktail' &
-    rlog_meta$Media == 'YPD' &
-    rlog_meta$Strain == 'WT')
-
-get_sample_mean = function(x) apply(x,1,mean)
+    rlog_meta$Drug == 'Cocktail')
 
 control_noDrug = get_sample_mean(rlog[,control_noDrug])
-condition_noDrug = get_sample_mean(rlog[,condition_noDrug])
 control_Drug = get_sample_mean(rlog[,control_Drug])
-condition_Drug = get_sample_mean(rlog[,condition_Drug])
 
-c = cor(condition_Drug   - control_Drug,
-        condition_noDrug - control_noDrug)
+availability_table = data.frame(rlog_meta) %>% dplyr::select(Stress, Drug) %>% table
+have_data = apply(availability_table,1,min) != 0
+conditions = rownames(availability_table)[have_data]
+# now for heatshock
+plot_comp = function(condition = 'Heatshock') {
+    condition_noDrug = which(
+        rlog_meta$Stress == condition &
+        rlog_meta$Drug == 'None')
 
-plot(condition_Drug   - control_Drug,
-     condition_noDrug - control_noDrug)
+    condition_Drug = which(
+        rlog_meta$Stress == condition &
+        rlog_meta$Drug == 'Cocktail')
 
+    condition_noDrug = get_sample_mean(rlog[,condition_noDrug])
+    condition_Drug = get_sample_mean(rlog[,condition_Drug])
+
+    drug = condition_Drug - control_Drug
+    noDrug = condition_noDrug - control_noDrug
+
+    c = cor(drug, noDrug)
+
+    plot(noDrug, # X
+         drug, # Y
+         xlab='FC without Drug',
+         ylab='FC with Drug',
+         main = paste('Comparing ', condition, ' vs. YPD: with and without Drug. cor = ', c)
+         )
+    abline(0,1)
+}
+
+pdf('drug_normalization.pdf')
+lapply(conditions, plot_comp)
+dev.off()
 
 
 
