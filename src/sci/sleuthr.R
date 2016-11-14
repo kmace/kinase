8c0b5a16eef0ca5729fe87de912f2838e1313e11
Kieran Mace <kieran.mace@gmail.com>
2016-11-14 08:16:55 -0800

    added code to sleuth to get it going with new data. does not work yet, but should after base Rdata file is recreated.

--- src/sci/sleuthr.R
+++ src/sci/sleuthr.R
@@ -1,31 +1,24 @@
-meta = read.table('../meta/Pincus/hiseq_info.txt', header=T, stringsAsFactors=FALSE, sep = '\t')
-meta = meta %>%
-    separate(Barcode,c('Tnum','bar'),sep='-') %>%
-    mutate(filename = paste(bar,'-s_',as.character(Position),'_1_sequence.txt.tar.gz',sep = ''),
-           path = file.path("../data/Pincus_data/results",filename)) %>%
-    select(sample = SampleName,
-           path)  %>%
-    separate(sample,c('Strain','Drug','Condition'),sep='_',remove=FALSE) %>%
-    mutate(Strain = relevel(factor(Strain),'WT'),
-           Drug = relevel(factor(Drug),'NO'),
-           Condition = relevel(factor(Condition),'NO'))
+source('../utils/load_libraries.R')
+source('../utils/load_functions.R')
+source('../utils/load_data.R')
+options(mc.cores = 4L)
+raw_counts = raw_counts[ ,meta$Strain=='WT']
+meta = meta[meta$Strain=='WT',]
 
-    #unite(Exp,c(Drug,Condition),sep='_', remove=FALSE)
+meta$sample = meta$Sample_Name
+meta$path = meta$kallisto_path
+# Get simple:
 
+meta = meta[!(meta$Stress != 'None' & meta$Drug != 'None'),]
+meta$Condition= relevel(factor(meta$Condition),"None_YPD_None")
 
-
-mart <- biomaRt::useMart(biomart = "ensembl", dataset = "scerevisiae_gene_ensembl")
-mart
-t2g <- biomaRt::getBM(attributes = c("ensembl_transcript_id", "ensembl_gene_id",
-    "external_gene_name"), mart = mart)
-
-
-t2g <- dplyr::rename(t2g, target_id = ensembl_transcript_id,
-  ens_gene = ensembl_gene_id, ext_gene = external_gene_name)
-
-model = model.matrix(~ 1 + Strain + Drug + Condition, data=meta)
+model = model.matrix(~ 1 + Condition, data=meta)
 so <- sleuth_prep(meta, model, target_mapping = t2g)
 so <- sleuth_fit(so)
+
+for (c in levels(meta$Condition)[-1]){
+  so = sleuth_wt(so, which_beta = paste('Condition',c, sep=''))
+}
 so <- sleuth_wt(so, which_beta = 'ConditionHS')
 #so <- sleuth_wt(so, which_beta = 'Drug1NM')
 sleuth_live(so)
