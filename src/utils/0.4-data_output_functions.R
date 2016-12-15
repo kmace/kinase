a9571a665228b65fb916b86ee24fd431e2d05d4f
Kieran Mace <kieran.mace@gmail.com>
2016-12-14 20:17:06 -0800

    implimented a function to create gct files

--- src/utils/0.4-data_output_functions.R
+++ src/utils/0.4-data_output_functions.R
@@ -1,30 +1,40 @@
-writeGCT <- function (gct,
-                      filename,
-                      check.file.extension = TRUE) {
-    if (check.file.extension) {
-        filename <- .checkExtension (filename, ".gct")
-    }
+writeGCT <- function (data,
+                      row_metadata,
+                      col_metadata,
+                      filename) {
+    nrows = dim(data)[1]
+    ncols = dim(data)[2]
+    row_metadata_fields = dim(row_metadata)[2]
+    col_metadata_fields = dim(col_metadata)[2]
     f <- file (filename, "w")
     on.exit (close (f))
     cat ("#1.3", "\n", file = f, append = TRUE, sep = "")
-    cat (dim (gct$data)[1], "\t", dim(gct$data)[2], "\n", file = f,
+    cat (nrows, "\t",
+         ncols, "\t",
+         row_metadata_fields, "\t",
+         col_metadata_fields, "\n", file = f,
          append = TRUE, sep = "")
-    cat ("Name", "\t", file = f, append = TRUE, sep = "")
-    cat ("Description", file = f, append = TRUE, sep = "")
-    names <- colnames (gct$data)
+    # First row
+    cat ("Gene/id", file = f, append = TRUE, sep = "")
+    names <- colnames(row_metadata)
+    for (j in 1:length(names)) {
+        cat ("\t", names[j], file = f, append = TRUE, sep = "")
+    }
+    names <- colnames(data)
     for (j in 1:length (names)) {
         cat ("\t", names[j], file = f, append = TRUE, sep = "")
     }
     cat ("\n", file = f, append = TRUE, sep = "")
-    m <- matrix (nrow = dim (gct$data)[1], ncol = 2)
-    m[, 1] <- row.names (gct$data)
-    if (!is.null (gct$row.descriptions)) {
-        m[, 2] <- gct$row.descriptions
-    } else {
-        m[, 2] <- ""
-    }
-    m <- cbind (m, gct$data)
+    # Build col metadata
+    names = colnames(col_metadata)
+    nas = matrix(NA, nrow= col_metadata_fields, ncol=row_metadata_fields  )
+    col_metadata_matrix = cbind(names, nas, t(as.data.frame(lapply(col_metadata,as.character))))
+    write.table(col_metadata_matrix, file = f, append = TRUE, quote = FALSE, sep = "\t",
+                 eol = "\n", col.names = FALSE, row.names = FALSE)
+    # Build row metadata
+    names = rownames(data)
+    m <- cbind (names, row_metadata, data)
     write.table (m, file = f, append = TRUE, quote = FALSE, sep = "\t",
                  eol = "\n", col.names = FALSE, row.names = FALSE)
-    filename
 }
+writeGCT(data,row_metadata,col_metadata,'test.gct')
