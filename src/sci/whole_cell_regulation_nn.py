259954ee8b2e3be0c2d7abf2e6c90654a3a245cd
Kieran Mace <kieran.mace@gmail.com>
2017-01-08 21:33:37 -0800

    added script for first attempt at nn on gene expression

--- src/sci/whole_cell_regulation_nn.py
+++ src/sci/whole_cell_regulation_nn.py
@@ -2,8 +2,10 @@ import numpy as np
 import pandas as pd
 # In[38]:
 expression = pd.read_csv('../../input/nn/data.csv')
+genes = expression["Unnamed: 0"]
 del expression["Unnamed: 0"] # This holds the gene names just so you know...
 meta = pd.read_csv('../../input/nn/meta.csv')
+experiments = meta["Unnamed: 0"]
 del meta["Unnamed: 0"]
 # In[39]:
 # expression
@@ -20,19 +22,46 @@ target = np.array(expression, dtype='float32')
 data = stress_one_hot
 data = np.array(data, dtype='float32')
 
-adam = tflearn.Adam(learning_rate=0.000001, beta1=0.99)
+
+num_stresses = 9
+num_kinase = 32
+num_transcription_factors = 100
+num_genes = 6692
 
 # Build neural network
-net = tflearn.input_data(shape=[None, 9])
-net = tflearn.fully_connected(net, 32)
-net = tflearn.fully_connected(net, 6692)
+stress = tflearn.input_data(shape=[None, num_stresses])
+kinase = tflearn.fully_connected(stress, num_kinase)
+transcription_factor = tflearn.fully_connected(kinase, num_transcription_factors)
+gene = tflearn.fully_connected(transcription_factor, num_genes)
 
-regression = tflearn.regression(net, optimizer=adam, loss='mean_square')
+adam = tflearn.Adam(learning_rate=0.001, beta1=0.99)
+
+regression = tflearn.regression(gene, optimizer=adam, loss='mean_square')
 
 
 # Define model
-model = tflearn.DNN(net)
+model = tflearn.DNN(regression, tensorboard_verbose=3)
 # Start training (apply gradient descent algorithm)
-model.fit(data, target, n_epoch=100000, batch_size=300, show_metric=True)
+model.fit(data, target, n_epoch=1000, batch_size=441, show_metric=True)
 
-# Let's create some data for DiCaprio and Winslet
+df = pd.DataFrame()
+df['nn_beta'] = model.get_weights(gene.b)
+df['gene_mean'] = np.mean(expression,0)
+fig1 = plt.figure()
+sns.lmplot('nn_beta','gene_mean',data=df)
+fig1.show()
+
+stress_to_kinase = model.get_weights(kinase.W)
+fig2 = plt.figure()
+sns.clustermap(stress_to_kinase)
+fig2.show()
+
+kinase_to_tf = model.get_weights(transcription_factor.W)
+fig3 = plt.figure()
+sns.clustermap(kinase_to_tf)
+fig3.show()
+
+tf_to_gene = model.get_weights(gene.W)
+fig4 = plt.figure()
+sns.clustermap(tf_to_gene)
+fig4.show()
