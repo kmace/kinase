6b9bce26d4a65c1cc4133a2bfa5c2abdd03e50d1
Kieran Mace <kieran.mace@gmail.com>
2017-01-30 10:18:59 -0800

    Installation script, plus cleanup
    Added a script to install all R dependancies, and cleaned up permission of a script, as well as adding a gitignore for the models dir

---
+++ src/utils/install_libraries.R
@@ -0,0 +1,37 @@
+list.of.cran.packages = c(
+'devtools',
+'dplyr',
+'plyr',
+'ggplot2',
+'ggrepel',
+'gplots',
+'lattice',
+'mclust',
+'pheatmap',
+'PoiClaClu',
+'RColorBrewer',
+'reshape2',
+'tidyr',
+'UpSetR',
+'viridis',
+'vsn'
+)
+
+list.of.bioc.packages = c(
+'biomaRt',
+'DESeq2',
+'genefilter',
+'rhdf5'
+)
+
+new.packages <- list.of.cran.packages[!(list.of.cran.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+#Poisson Distance (Witten 2011), implemented in the PoiClaClu package. This measure of dissimilarity between counts also takes the inherent variance structure of counts into consideration when calculating the distances between samples
+
+
+source("http://bioconductor.org/biocLite.R")
+biocLite()
+new.packages <- list.of.bioc.packages[!(list.of.bioc.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) biocLite(new.packages)
+
+devtools::install_github("pachterlab/sleuth")
