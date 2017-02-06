8cd0c1370da706c47444e773bc0a726d87cc654f
Kieran Mace <kieran.mace@gmail.com>
2017-02-06 15:26:39 -0800

    Introduced example of the Tunicamycin plot. need to functionalize for general use

--- src/sci/detect_kinase.R
+++ src/sci/detect_kinase.R
@@ -54,3 +54,34 @@ for(c in unique(meta$Condition)){
   temp_meta = meta[temp_idx,]
   make_plot(temp_data, temp_meta, title=c)
 }
+
+
+# Example of Tunicamycin Plot
+x = assay(vsd)
+x = x - rowMeans(x) # Can we do this in a better way?
+colnames(x) = meta$Sample_Name
+t = vsd[,colData(vsd)$Stress == 'Tunicamycin']
+tx = assay(t)
+tm = colData(t)
+rm = rowMedians(tx[,tm$Strain=='WT'])
+dec = order(rm, decreasing = T)
+tx = tx[dec,] # Can we also subset these so that we only keep genes that are actually on? what about DE?
+rm = rm[dec]
+colnames(tx) = tm$Strain_Code
+
+sd = apply(msm,1,sd)
+
+sigma = apply(tx,2,function(x) (x-rm)/sd)
+sigma = rename_gene_names(sigma,t2g = t2g)
+sigma[sigma < 1 & sigma > -1] = 0
+
+pheatmap(sigma[1:30,]) # again, lets have this be subset, not some dumb number
+pheatmap(sigma[1:300,])
+pheatmap(sigma[1:1000,])
+
+
+# write to GCT for morpheus
+sgn = data.frame(sigma)
+t = t2g[match(rownames(sgn),t2g$name),]
+tmm = tm[,1:9]
+writeGCT(sgn, t, tmm, 'Tunicamycin_2_sigma.gct')
