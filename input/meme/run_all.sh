5e1ace54d2f02159a507fa349b83fd5fde794d89
Kieran Mace <kieran.mace@gmail.com>
2017-05-23 13:34:31 -0700

    added new files for running meme

---
+++ input/meme/run_all.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+  
+parallel bash run_meme.sh {} ::: ../kinase/output/Strain_Dependent_Genes/*.txt
+
+for a in *_sig*.fasta do 
+	b=`basename $a .fasta`; 
+	mv $a $b; 
+done
+
+for a in *sig; do 
+	b=`basename $a _sig`; 
+	mkdir $b; 
+	mv ${a}* $b; 
+done
