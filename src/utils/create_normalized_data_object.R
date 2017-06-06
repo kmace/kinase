c33941fca12834cc6247f9396702b7b9a797f33e
Kieran Mace <kieran.mace@gmail.com>
2017-06-06 09:05:50 -0700

    modified creation of normalized data.

--- src/utils/create_normalized_data_object.R
+++ src/utils/create_normalized_data_object.R
@@ -4,26 +4,38 @@ source('../utils/load_functions.R')
 
 t2g = load_transcripts_to_genes()
 meta = get_sample_meta()
+
+meta$Strain = relevel(factor(meta$Strain), 'WT')
+meta$Drug = relevel(factor(meta$Drug), 'None')
+meta$Stress = relevel(factor(meta$Stress), 'None')
+meta$Media = relevel(factor(meta$Media), 'YPD')
+meta = meta %>% mutate(Condition = if_else(Stress=='None',
+                                           as.character(Media),
+                                           as.character(Stress)
+                                           ))
+meta$Condition = relevel(factor(meta$Condition), 'YPD')
+
 raw_counts = load_count_matrix(meta,'star')
 
-dds = DESeqDataSetFromMatrix(countData = raw_counts, colData = meta, ~1)
 
-colData(dds)$Strain = relevel(factor(colData(dds)$Strain), 'WT')
-colData(dds)$Drug = relevel(factor(colData(dds)$Drug), 'None')
-colData(dds)$Stress = relevel(factor(colData(dds)$Stress), 'None')
-colData(dds)$Media = relevel(factor(colData(dds)$Media), 'YPD')
+dds = DESeqDataSetFromMatrix(countData = raw_counts, colData = meta, ~ Condition + Strain)
+dds = dds[,dds$Experimenter=='Kieran']
+dds = dds[,dds$Drug == 'Cocktail']
 
-#rld_all <- rlogTransformation(dds, blind=TRUE)
-#rlog = assay(rld_all)
-#rlog_meta = colData(rld_all)
 
-dds <- estimateSizeFactors(dds)
-baseMean <- rowMeans(counts(dds, normalized=TRUE))
-sum(baseMean > 1)
-idx <- sample(which(baseMean > 5), 1000)
-dds.sub <- dds[idx, ]
-dds.sub <- estimateDispersions(dds.sub)
-dispersionFunction(dds) <- dispersionFunction(dds.sub)
-vsd <- varianceStabilizingTransformation(dds, blind=FALSE)
+dds = DESeq(dds, parallel = TRUE)
+rlt = rlogTransformation(dds, blind = FALSE) # This takes a long long time
+rlog = assay(rlt)
+meta = colData(rlt)
+
+
+# dds <- estimateSizeFactors(dds)
+# baseMean <- rowMeans(counts(dds, normalized=TRUE))
+# sum(baseMean > 1)
+# idx <- sample(which(baseMean > 5), 1000)
+# dds.sub <- dds[idx, ]
+# dds.sub <- estimateDispersions(dds.sub)
+# dispersionFunction(dds) <- dispersionFunction(dds.sub)
+# vsd <- varianceStabilizingTransformation(dds, blind=FALSE)
 
 save.image('../../input/images/normalized_data.RData')
