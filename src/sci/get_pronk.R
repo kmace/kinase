83707316bd40b9f5eed66b38f6867d16006bff96
Kieran Mace <kieran.mace@gmail.com>
2017-06-06 10:40:47 -0700

    cleaning up sci dir

--- src/sci/get_pronk.R
+++
@@ -1,9 +0,0 @@
-library(Biobase)
-library(GEOquery)
-gset <- getGEO("GSE11452", GSEMatrix =TRUE, getGPL=FALSE)
-if (length(gset) > 1) idx <- grep("GPL90", attr(gset, "names")) else idx <- 1
-gset <- gset[[idx]]
-meta = pData(gset)
-px = exprs(gset)
-p2t = read.table('../../input/reference/GPL_to_ORF.tsv', fill=T, header=T, stringsAsFactors=F)
-rownames(px) = p2t[,2]
