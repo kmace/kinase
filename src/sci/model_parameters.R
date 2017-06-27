0b02220fbb4a9ecafe3e6721c2f6407f1aaa6032
Kieran Mace <kieran.mace@gmail.com>
2017-06-27 14:36:57 -0700

    updated params script

--- src/sci/model_parameters.R
+++ src/sci/model_parameters.R
@@ -6,6 +6,8 @@ library(tidyr)
 library(dplyr)
 library(broom)
 library(tibble)
+library(heatmap3)
+library(ggplot2)
 
 vlog = vlog - rowMeans(vlog)
 row_sd = apply(vlog,1,sd)
@@ -13,26 +15,34 @@ vlog = apply(vlog,2,function(x) x / row_sd)
 
 vlog = as.data.frame(vlog)
 vlog$Gene = rownames(vlog)
-measurements = gather(vlog, Experiment, Expression, -Gene)
+measurements = gather(vlog, Sample_Name, Expression, -Gene)
 # should be a left join on the meta subsetted
-measurements = measurements %>% 
-  mutate(Strain = meta[Experiment, ]$Strain,
-         Condition = meta[Experiment, ]$Condition) %>%
-  select(-Experiment)
-              
-fits = measurements %>% 
+meta_sub = as.data.frame(meta) %>% select(Sample_Name, Condition, Strain)
+
+measurements = left_join(measurements, meta_sub)
+
+measurements = measurements %>%
+  group_by(Gene) %>%
+  mutate(Gene_range = diff(range(Expression)),
+         Gene_sd = sd(Expression),
+         Gene_mean = mean(Expression),
+         Norm_Expression = (Expression - Gene_mean) / Gene_sd) %>%
+  ungroup() %>%
+  arrange(Gene)
+
+measurements = measurements %>% filter(Gene_range > 1)
+
+fits = measurements %>%
   dplyr::group_by(Gene) %>%
-  dplyr::do(fitGene = lm(Expression ~ Strain + Condition, data = .)) %>%
+  dplyr::do(fitGene = lm(Norm_Expression ~ Strain + Condition, data = .)) %>%
   broom::tidy(fitGene)
 
 parameters = fits %>% select(Gene, term, estimate) %>% spread(term, estimate) %>% ungroup()
 rownames(parameters) = parameters$Gene
-parameters = parameters[,-Gene]
 
 parameter_matrix = as.matrix(select(parameters, -Gene))
-col_meam = apply(mat,2,mean)
-#m = apply(mat,1,function(x) x - col_m)
+col_mean = apply(parameter_matrix,2,mean)
+#m = apply(parameter_matrix,1,function(x) x - col_m)
 #m = t(m)
 #heatmap3(m[m[,10]>1,-10], labRow = NA, scale='none')
-heatmap3(mat, labRow = NA, scale='none')
-
+heatmap3(parameter_matrix, labRow = NA, scale='none')
