71bdeb17633fc3a066eb49761aca42743b3376eb
Kieran Mace <kieran.mace@gmail.com>
2017-07-23 18:59:58 -0700

    working version of full modeling

--- src/sci/model_parameters.R
+++ src/sci/model_parameters.R
@@ -1,64 +1,53 @@
-rm(list=ls())
+    rm(list=ls())
 
-load('../../input/images/normalized_data.RData')
+    load('../../input/images/normalized_data.RData')
 
-library(tidyr)
-library(purrr)
-library(dplyr)
-library(broom)
-library(tibble)
-library(heatmap3)
-library(ggplot2)
-#library(multidplyr)
+    library(tidyr)
+    library(purrr)
+    library(dplyr)
+    library(broom)
+    library(tibble)
+    library(heatmap3)
+    library(ggplot2)
+    #library(multidplyr)
 
-#vlog = vlog - rowMeans(vlog)
-#row_sd = apply(vlog,1,sd)
-#vlog = apply(vlog,2,function(x) x / row_sd)
+    #vlog = vlog - rowMeans(vlog)
+    #row_sd = apply(vlog,1,sd)
+    #vlog = apply(vlog,2,function(x) x / row_sd)
 
-vlog = as.data.frame(vlog)
-vlog$Gene = rownames(vlog)
-measurements = gather(vlog, Sample_Name, Expression, -Gene)
-# should be a left join on the meta subsetted
-meta_sub = as.data.frame(meta) %>% select(Sample_Name, Condition, Strain)
+    vlog = as.data.frame(vlog)
+    vlog$Gene = rownames(vlog)
+    measurements = gather(vlog, Sample_Name, Expression, -Gene)
+    # should be a left join on the meta subsetted
+    meta_sub = as.data.frame(meta) %>% select(Sample_Name, Condition, Strain)
 
-measurements = left_join(measurements, meta_sub)
+    measurements = left_join(measurements, meta_sub)
 
-f1 = Expression ~ Strain
-f2 = Expression ~ Condition
-f3 = Expression ~ Strain + Condition
+    lin_mod = function(formula) {
+      function(data,...){
+      map(data,~lm(formula, data = .))
+      }
+    }
 
-lin_mod = function(formula) {
-  function(data) {
-    lm(formula, data = data)
-  }
-}
+    list_model <- list(Strain_model= Expression ~ Strain,
+                       Condition_model= Expression ~ Condition,
+                       full_model= Expression ~ Strain + Condition) %>%
+                  lapply(lin_mod)
 
-strain_lm = lin_mod(f1)
-condition_lm = lin_mod(f2)
-full_lm = lin_mod(f3)
+    genes = measurements %>%
+      group_by(Gene) %>%
+      mutate(Gene_range = diff(range(Expression)),
+             Gene_sd = sd(Expression),
+             Gene_mean = mean(Expression),
+             Norm_Expression = (Expression - Gene_mean) / Gene_sd) %>%
+      filter(Gene_range > 1) %>%
+      nest() %>%
+      mutate_at(.vars=("data"),.funs=list_model) %>%
+      mutate_at(.vars=vars(ends_with('model')), .funs = funs(aug = map(.,augment))) %>%
+      mutate_at(.vars=vars(ends_with('aug')), .funs = funs(resid = map(.,'.resid'))) %>%
+      arrange(Gene)
 
 
-genes = measurements %>%
-  group_by(Gene) %>%
-  mutate(Gene_range = diff(range(Expression)),
-         Gene_sd = sd(Expression),
-         Gene_mean = mean(Expression),
-         Norm_Expression = (Expression - Gene_mean) / Gene_sd) %>%
-  filter(Gene_range > 1) %>%
-  nest() %>%
-  mutate(fullModel = map(data, full_lm),
-         conditionModel = map(data, condition_lm),
-         strainModel = map(data, strain_lm),
-         augment_fullModel = fullModel %>% map(augment),
-         augment_conditionModel = conditionModel %>% map(augment),
-         augment_strainModel = strainModel %>% map(augment),
-         full_residual = augment_fullModel %>% map_dbl(".resid"),
-         condition_residual = augment_conditionModel %>% map_dbl(".resid"),
-         strain_residual = augment_strainModel %>% map_dbl(".resid")) %>%
-         arrange(Gene)
-
-  #unnest(data, full_residual, condition_residual, strain_residual) #%>%
-  #collect()
 
 model_parameter_stats = fits %>%
   broom::tidy(Model)
