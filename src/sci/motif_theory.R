013dd3524f430c15518ab8b4073a2616b3eac20d
Kieran Mace <kieran.mace@gmail.com>
2017-09-20 15:12:07 -0700

    update of logic

--- src/sci/motif_theory.R
+++ src/sci/motif_theory.R
@@ -3,11 +3,15 @@ library(tibble)
 
 regulation = c('+','0', '-')
 kinase_state = c('on', 'off')
+kinase_type = c('WT', 'AS')
+#NMPP1 = TRUE
 
 possible_motifs = expand.grid(C_on_G = regulation,
 							  C_on_K = regulation,
 							  K_on_G = regulation,
-							  K_base_state = kinase_state, stringsAsFactors = F) %>% as_tibble
+							  K_type = kinase_type,
+							  K_base = kinase_state,
+							  stringsAsFactors = F) %>% as_tibble #%>% group_by_at(vars(-K_type))
 
 regulation_to_state = function(reg){
 	if(reg=='+'){
@@ -34,21 +38,50 @@ flip_reg = function(reg){
 flip_reg = Vectorize(flip_reg)
 
 possible_motifs %>%
-mutate(# Condition Specific Steps
-	   Condition_affects_Kinase = C_on_K != '0',
-	   Kinase_sensing_state = if_else(Condition_affects_Kinase, regulation_to_state(C_on_K), K_base_state),
-	   Condition_modified_state = K_base_state != Kinase_sensing_state,
+  # Specify who may act:
+  mutate(
+     C_acts_on_G = C_on_G != '0',
+	   C_acts_on_K = C_on_K != '0',
+     D_acts_on_K = K_type == 'AS',  # Analog Sensitive
+     K_acts_on_G = K_on_G != '0') %>%
+  # Account for Kinase modifications and tracking
+	mutate(
+	   # Condition Specific Steps
+	   K_sense = if_else(C_acts_on_K, regulation_to_state(C_on_K), K_base), # Kinase state after Condition
+	   C_changed_K = K_base != K_sense, # This is what we want to detect, when this is true
 	   # Analog Specific steps
-	   Analog_affects_Kinase = TRUE,
-	   Kinase_numb_state = 'off',
-	   Analog_modified_state = Kinase_sensing_state != Kinase_numb_state,
-	   # Possible outcomes
-	   General_condition_effect = C_on_G,
-	   Gener = 
-	   synopsis = paste('Condition causes gene to go in a', C_on_G, 'direction.\n',
-	   					'With no analog, Kinase is in', Kinase_sensing_state, 'state, which causes the gene to go in a ', K_on_G, 'direciton when on.\n',
-					     'Kinase was basally', K_base_state, 'and',
-						 if_else(Condition_modified_state, 'was', 'was not'), 'flipped by the condition, and',
-					     if_else(Analog_modified_state, 'was', 'was not'), 'flipped by NMPP1 (to off).', sep=' ')
+	   K_final = if_else(D_acts_on_K, 'off', K_sense),
+	   D_changed_K = K_sense != K_final,
+	   # Final checks
+	   K_changed = K_base != K_final) %>%
+  # Figure out if something changed, and who was responsible for it
+  mutate(
+    C_acts_on_G_through_K = K_acts_on_G & C_changed_K,
+    D_blocks_C_action_on_G_through_K = C_acts_on_G_through_K & D_changed_K,
+    D_redundant_because_C = C_acts_on_G_through_K & !D_changed_K,
+    D_acts_on_G_generally = D_acts_on_K & K_base == 'on' & K_acts_on_G
+  ) %>%
+  # Account for changes in Gene Expression in the final state
+  mutate(
+    C_contribution_to_G = C_on_G,
+    K_contribution_to_G = if_else(K_final == 'on', K_on_G, flip_reg(K_on_G)),
+    K_transitions = paste(K_base, K_sense, K_final, sep = '->'),
+    G_movment = paste(C_contribution_to_G, K_contribution_to_G, sep='/')
+    ) %>%
+  mutate(
+    synopsis = paste('C has direct effect:', C_acts_on_G,
+                     'C has effect through K:', C_acts_on_G_through_K,
+                     'D has general effect:', D_acts_on_G_generally,
+                     'D reverses C\'s effect:', D_blocks_C_action_on_G_through_K,
+                     'D redundant because C:', D_redundant_because_C,
+                     'Kinase Transitions:', K_transitions,
+                     sep = '\n')
+  ) %>% View()
+
+	   #synopsis = paste('Condition causes gene to go in a', C_on_G, 'direction.\n',
+	   #					'With no analog, Kinase is in', Kinase_sensing_state, 'state, which causes the gene to go in a ', K_on_G, 'direciton when on.\n',
+		#			     'Kinase was basally', K_base_state, 'and',
+		#				 if_else(Condition_modified_state, 'was', 'was not'), 'flipped by the condition, and',
+		#			     if_else(Analog_modified_state, 'was', 'was not'), 'flipped by NMPP1 (to off).', sep=' ')
 	   # NMPP1 either can break the basal level, or break condition signalling!
-   )
+   #)
