4e4638fc73471077b1188ff9dde95d257d311ef6
Kieran Mace <kieran.mace@gmail.com>
2017-09-20 17:59:34 -0700

    theory update. removed possibility for K_type. it is always AS

--- src/sci/motif_theory.R
+++ src/sci/motif_theory.R
@@ -3,13 +3,11 @@ library(tibble)
 
 regulation = c('+','0', '-')
 kinase_state = c('on', 'off')
-kinase_type = c('WT', 'AS')
 #NMPP1 = TRUE
 
 possible_motifs = expand.grid(C_on_G = regulation,
 							  C_on_K = regulation,
 							  K_on_G = regulation,
-							  K_type = kinase_type,
 							  K_base = kinase_state,
 							  stringsAsFactors = F) %>% as_tibble #%>% group_by_at(vars(-K_type))
 
@@ -37,47 +35,73 @@ flip_reg = function(reg){
 
 flip_reg = Vectorize(flip_reg)
 
+reg_to_num = function(reg){
+  if(reg=='+'){
+    return(1)
+  } else if(reg=='-'){
+    return(0)
+  } else if(reg=='0'){
+    return(0)
+  }
+}
+
+reg_to_num = Vectorize(reg_to_num)
 possible_motifs %>%
   # Specify who may act:
   mutate(
      C_acts_on_G = C_on_G != '0',
 	   C_acts_on_K = C_on_K != '0',
-     D_acts_on_K = K_type == 'AS',  # Analog Sensitive
      K_acts_on_G = K_on_G != '0') %>%
   # Account for Kinase modifications and tracking
 	mutate(
 	   # Condition Specific Steps
 	   K_sense = if_else(C_acts_on_K, regulation_to_state(C_on_K), K_base), # Kinase state after Condition
-	   C_changed_K = K_base != K_sense, # This is what we want to detect, when this is true
+	   #C_changed_K = K_base != K_sense, # This is what we want to detect, when this is true
 	   # Analog Specific steps
-	   K_final = if_else(D_acts_on_K, 'off', K_sense),
-	   D_changed_K = K_sense != K_final,
+	   K_final = 'off') %>% #,
+	   #D_changed_K = K_sense != K_final,
 	   # Final checks
-	   K_changed = K_base != K_final) %>%
+	   #K_changed = K_base != K_final,
+	   #K_flipped = C_changed_K | D_changed_K) %>%
   # Figure out if something changed, and who was responsible for it
   mutate(
-    C_acts_on_G_through_K = K_acts_on_G & C_changed_K,
-    D_blocks_C_action_on_G_through_K = C_acts_on_G_through_K & D_changed_K,
-    D_redundant_because_C = C_acts_on_G_through_K & !D_changed_K,
-    D_acts_on_G_generally = D_acts_on_K & K_base == 'on' & K_acts_on_G
+    C_acts_on_G_through_K_without_D = (K_base != K_sense) & K_acts_on_G,
+    D_acts_on_G_through_K_without_C = K_base == 'on' & K_acts_on_G,
+    D_blocks_C_action_on_G_through_K = C_acts_on_G_through_K_without_D & (K_sense == 'on'),
+    D_and_C_are_redundant = C_acts_on_G_through_K_without_D & (K_sense == 'off')
   ) %>%
   # Account for changes in Gene Expression in the final state
   mutate(
-    C_contribution_to_G = C_on_G,
-    K_contribution_to_G = if_else(K_final == 'on', K_on_G, flip_reg(K_on_G)),
-    K_transitions = paste(K_base, K_sense, K_final, sep = '->'),
-    G_movment = paste(C_contribution_to_G, K_contribution_to_G, sep='/')
+    K_transitions = paste(K_base, K_sense, K_final, sep = '->')
     ) %>%
   mutate(
-    synopsis = paste('C has direct effect:', C_acts_on_G,
-                     'C has effect through K:', C_acts_on_G_through_K,
-                     'D has general effect:', D_acts_on_G_generally,
-                     'D reverses C\'s effect:', D_blocks_C_action_on_G_through_K,
-                     'D redundant because C:', D_redundant_because_C,
-                     'Kinase Transitions:', K_transitions,
-                     sep = '\n')
-  ) %>% View()
-
+    synopsis = paste(paste('C has direct effect:', C_acts_on_G, sep = ' '),
+                     paste('C direct effect sign: ', C_on_G, sep = ' '),
+                     paste('D has a condition independent effect (K_base is on):', D_acts_on_G_through_K_without_C, sep = ' '),
+                     paste('D effect sign (through K_final being off):', flip_reg(K_on_G), sep = ' '),
+                     paste('C has an indirect effect through K (no D):', C_acts_on_G_through_K_without_D, sep = ' '),
+                     paste('C indirect effect through K (no D) sign:', paste(C_on_K, K_on_G, sep='/'), sep = ' '),
+                     paste('D blocks C effect:', D_blocks_C_action_on_G_through_K, sep = ' '),
+                     paste('D and C are redundant:', D_and_C_are_redundant, sep = ' '),
+                     paste('Kinase Transitions:', K_transitions, sep = ' '),
+                     paste('Linear Expectation (C, K, R):', paste0('(', C_on_G,
+                                                                   ', ', flip_reg(K_on_G),
+                                                                   ', 0)'), sep = ' '),
+                     paste('Complex Expectation (C, K, R):', paste0('(', C_on_G,
+                                                                    ', ', flip_reg(K_on_G),
+                                                                    ',', paste(C_on_K,
+                                                                               K_on_G,
+                                                                               sep='/') ,')'), sep = ' '),
+                      sep = '\n')
+    # g = purrr::map(., function(x) graph_from_data_frame(data.frame(from = c('c','c','k'),
+    #                                      to = c('k', 'g', 'g'),
+    #                                      strength = c(reg_to_num(x$C_on_K),
+    #                                                   reg_to_num(x$C_on_G),
+    #                                                   reg_to_num(x$K_on_G))),
+    #                           directed=T,
+    #                           vertices = nodes))
+    ) -> outcomes
+writeLines(outcomes$synopsis[2])
 	   #synopsis = paste('Condition causes gene to go in a', C_on_G, 'direction.\n',
 	   #					'With no analog, Kinase is in', Kinase_sensing_state, 'state, which causes the gene to go in a ', K_on_G, 'direciton when on.\n',
 		#			     'Kinase was basally', K_base_state, 'and',
