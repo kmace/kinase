e9208c14523056e31ae3597347e87c1bde0f3909
Kieran Mace <kieran.mace@gmail.com>
2017-09-25 13:13:25 -0700

    working on motif theory to see why residuals could be produced.

--- src/sci/motif_theory_2.R
+++ src/sci/motif_theory_2.R
@@ -1,36 +1,69 @@
 library(dplyr)
 library(tibble)
 
-regulation = c('+','0', '-')
-kinase_state = c('on', 'off')
-#NMPP1 = TRUE
 
-possible_motifs = expand.grid(C_on_G = regulation,
-							  C_on_K = regulation,
-							  K_on_G = regulation,
-							  K_base = kinase_state,
-							  stringsAsFactors = F) %>% as_tibble #%>% group_by_at(vars(-K_type))
+possible_motifs = expand.grid(Bg = c(0, 1),
+                              Bs = c(0, 1),
+                              Cg = c(-1, 0, 1),
+                              Cs = c(-1, 0, 1),
+                              Ds = c(-1, 0),
+                              #Kg_on_E = c(-1, 0, 1),
+                              #Ks_on_E = c(-1, 0, 1),
+                              stringsAsFactors = F) %>% as_tibble #%>% group_by_at(vars(-K_type))
+
+get_delta_E = function(df){
+  General_Term = get_general_term(df$Bg, df$Cg)#, df$Kg_on_E)
+  Specific_Term = get_specific_term(df$Bs, df$Cs, df$Ds)#, DF$Ks_on_E)
+  equation = paste0('(', General_Term, ' * Kg_on_E) + (', Specific_Term, '* Kg_on_E)')
+  return(equation)
+}
+
+  General_Term = get_general_term(Bg, Cg),#, Kg_on_E)
+  Specific_Term = get_specific_term(Bs, Cs, Ds),#, Ks_on_E)
+  equation = paste0('(', General_Term, ' * Kg_on_E) + (', Specific_Term, '* Kg_on_E)')
+
+
+
+get_general_term = function(Bg, Cg){
+if(Cg == 0){
+  return(0)
+} else {
+  return(Bg - Cg)
+}
+}
+
+get_specific_term = function(Bs, Cs, Ds){
+  if(Cs == 0 & Ds == 0){
+    return(0)
+  } else if(Ds == 0){
+    return(Bs - Cs)
+  } else {
+    return(Bs - Ds)
+  }
+}
+
+possible_motifs %>% mutate(equation = map())
 
 regulation_to_state = function(reg){
-	if(reg=='+'){
-		return('on')
-	} else if(reg=='-'){
-		return('off')
-	} else{
-		return(NA)
-	}
+  if(reg=='+'){
+    return('on')
+  } else if(reg=='-'){
+    return('off')
+  } else{
+    return(NA)
+  }
 }
 
 regulation_to_state = Vectorize(regulation_to_state)
 
 flip_reg = function(reg){
-	if(reg=='+'){
-		return('-')
-	} else if(reg=='-'){
-		return('+')
-	} else if(reg=='0'){
-		return('0')
-	}
+  if(reg=='+'){
+    return('-')
+  } else if(reg=='-'){
+    return('+')
+  } else if(reg=='0'){
+    return('0')
+  }
 }
 
 flip_reg = Vectorize(flip_reg)
@@ -49,20 +82,20 @@ reg_to_num = Vectorize(reg_to_num)
 possible_motifs %>%
   # Specify who may act:
   mutate(
-     C_acts_on_G = C_on_G != '0',
-	   C_acts_on_K = C_on_K != '0',
-     K_acts_on_G = K_on_G != '0') %>%
+    C_acts_on_G = C_on_G != '0',
+    C_acts_on_K = C_on_K != '0',
+    K_acts_on_G = K_on_G != '0') %>%
   # Account for Kinase modifications and tracking
-	mutate(
-	   # Condition Specific Steps
-	   K_sense = if_else(C_acts_on_K, regulation_to_state(C_on_K), K_base), # Kinase state after Condition
-	   #C_changed_K = K_base != K_sense, # This is what we want to detect, when this is true
-	   # Analog Specific steps
-	   K_final = 'off') %>% #,
-	   #D_changed_K = K_sense != K_final,
-	   # Final checks
-	   #K_changed = K_base != K_final,
-	   #K_flipped = C_changed_K | D_changed_K) %>%
+  mutate(
+    # Condition Specific Steps
+    K_sense = if_else(C_acts_on_K, regulation_to_state(C_on_K), K_base), # Kinase state after Condition
+    #C_changed_K = K_base != K_sense, # This is what we want to detect, when this is true
+    # Analog Specific steps
+    K_final = 'off') %>% #,
+  #D_changed_K = K_sense != K_final,
+  # Final checks
+  #K_changed = K_base != K_final,
+  #K_flipped = C_changed_K | D_changed_K) %>%
   # Figure out if something changed, and who was responsible for it
   mutate(
     C_acts_on_G_through_K_without_D = (K_base != K_sense) & K_acts_on_G,
@@ -73,7 +106,7 @@ possible_motifs %>%
   # Account for changes in Gene Expression in the final state
   mutate(
     K_transitions = paste(K_base, K_sense, K_final, sep = '->')
-    ) %>%
+  ) %>%
   mutate(
     synopsis = paste(paste('C has direct effect:', C_acts_on_G, sep = ' '),
                      paste('C direct effect sign: ', C_on_G, sep = ' '),
@@ -92,7 +125,7 @@ possible_motifs %>%
                                                                     ',', paste(C_on_K,
                                                                                K_on_G,
                                                                                sep='/') ,')'), sep = ' '),
-                      sep = '\n')
+                     sep = '\n')
     # g = purrr::map(., function(x) graph_from_data_frame(data.frame(from = c('c','c','k'),
     #                                      to = c('k', 'g', 'g'),
     #                                      strength = c(reg_to_num(x$C_on_K),
@@ -100,12 +133,12 @@ possible_motifs %>%
     #                                                   reg_to_num(x$K_on_G))),
     #                           directed=T,
     #                           vertices = nodes))
-    ) -> outcomes
+  ) -> outcomes
 writeLines(outcomes$synopsis[2])
-	   #synopsis = paste('Condition causes gene to go in a', C_on_G, 'direction.\n',
-	   #					'With no analog, Kinase is in', Kinase_sensing_state, 'state, which causes the gene to go in a ', K_on_G, 'direciton when on.\n',
-		#			     'Kinase was basally', K_base_state, 'and',
-		#				 if_else(Condition_modified_state, 'was', 'was not'), 'flipped by the condition, and',
-		#			     if_else(Analog_modified_state, 'was', 'was not'), 'flipped by NMPP1 (to off).', sep=' ')
-	   # NMPP1 either can break the basal level, or break condition signalling!
-   #)
+#synopsis = paste('Condition causes gene to go in a', C_on_G, 'direction.\n',
+#					'With no analog, Kinase is in', Kinase_sensing_state, 'state, which causes the gene to go in a ', K_on_G, 'direciton when on.\n',
+#			     'Kinase was basally', K_base_state, 'and',
+#				 if_else(Condition_modified_state, 'was', 'was not'), 'flipped by the condition, and',
+#			     if_else(Analog_modified_state, 'was', 'was not'), 'flipped by NMPP1 (to off).', sep=' ')
+# NMPP1 either can break the basal level, or break condition signalling!
+#)
