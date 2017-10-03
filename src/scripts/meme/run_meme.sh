4648153f95243496b1806414a57bbf5893f3f621
Kieran Mace <kieran.mace@gmail.com>
2017-10-03 16:37:21 -0700

    updated meme code

--- src/scripts/meme/run_meme.sh
+++ src/scripts/meme/run_meme.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 query_file=$1
 file_base=`basename $1 .txt`
+output_dir=${2}/${file_base}
+mkdir -p ${output_dir}
 query_genes=`cat $query_file | perl -pe 'chomp if eof' | tr '\n' '|'`
-cat all_sc_promoters.fasta | paste -d ':' - - | grep -E $query_genes | tr ':' '\n' > ${file_base}.fasta
-../meme/bin/ame --verbose 1 --oc ./${file_base} --control all_sc_promoters.fasta --bgformat 1 --scoring avg --method ranksum --pvalue-report-threshold 0.05 ${file_base}.fasta JASPAR_CORE_2016_fungi.meme
+cat input/meme/all_sc_promoters.fasta | paste -d ':' - - | grep -E $query_genes | tr ':' '\n' > ${output_dir}/${file_base}.fasta
+~/meme/bin/ame --verbose 1 --oc ${output_dir}/${file_base} --control input/meme/all_sc_promoters.fasta --bgformat 1 --scoring avg --method ranksum --pvalue-report-threshold 0.05 ${output_dir}/${file_base}.fasta input/meme/JASPAR_CORE_2016_fungi.meme
