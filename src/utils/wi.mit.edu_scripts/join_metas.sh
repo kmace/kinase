c03a57cd8d888e346612656a0ffaa40a7f1c3d95
Kieran Mace <kieran.mace@gmail.com>
2017-10-03 14:41:29 -0700

    fixed up src directory

--- src/utils/wi.mit.edu_scripts/join_metas.sh
+++
@@ -1,9 +0,0 @@
-# Should handle the first line differently,
-# also, shold only keep one of the first lines
-
-awk '{print FILENAME, $0}' */meta/*.xls > all_meta.tsv
-
-#for metafile in */meta/*.xls; do 
-#    echo $a; 
-#    
-#done
--- src/utils/wi.mit.edu_scripts/put_on_vort.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-rsync -zaP kieran/ hes_share@vort.wi.mit.edu:/home/hes_share/kieran --delete
