ae3b1975a06ff88d35e18f3d62017841f143a586
Kieran Mace <kieran@gpucluster.ucsf.edu>
2017-10-04 13:34:41 -0700

    fixed up the data hadling script, to also call meme. and updated meme scripts

--- src/scripts/meme/run_all.sh
+++ src/scripts/meme/run_all.sh
@@ -5,4 +5,4 @@ output_dir=$2
 
 mkdir -p ${output_dir}
 
-parallel bash src/scripts/meme/run_meme.sh {} ${output_dir} ::: ${src_dir}/*.txt
+parallel -j20 bash src/scripts/meme/run_meme.sh {} ${output_dir} ::: ${src_dir}/*.txt
--- src/scripts/meme/run_meme.sh
+++ src/scripts/meme/run_meme.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 query_file=$1
 file_base=`basename $1 .txt`
-output_dir=${2}/${file_base}
-mkdir -p ${output_dir}
+output_dir=${2}
+
+mkdir -p ${output_dir}/fasta
+mkdir -p ${output_dir}/meme
+mkdir -p ${output_dir}/tfs
+
 query_genes=`cat $query_file | perl -pe 'chomp if eof' | tr '\n' '|'`
-cat input/meme/all_sc_promoters.fasta | paste -d ':' - - | grep -E $query_genes | tr ':' '\n' > ${output_dir}/${file_base}.fasta
-~/meme/bin/ame --verbose 1 --oc ${output_dir}/${file_base} --control input/meme/all_sc_promoters.fasta --bgformat 1 --scoring avg --method ranksum --pvalue-report-threshold 0.05 ${output_dir}/${file_base}.fasta input/meme/JASPAR_CORE_2016_fungi.meme
+cat input/meme/all_sc_promoters.fasta | paste -d ':' - - | grep -E $query_genes | tr ':' '\n' > ${output_dir}/fasta/${file_base}.fasta
+
+~/meme/bin/ame --verbose 1 --oc ${output_dir}/meme/${file_base} --control input/meme/all_sc_promoters.fasta --bgformat 1 --scoring avg --method ranksum --pvalue-report-threshold 0.05 ${output_dir}/fasta/${file_base}.fasta input/meme/JASPAR_CORE_2016_fungi.meme
+
+src/scripts/meme/get_TFs.sh ${output_dir}/meme/${file_base}/ame.html > ${output_dir}/tfs/${file_base}_tfs.txt
