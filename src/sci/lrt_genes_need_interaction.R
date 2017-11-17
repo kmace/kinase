d01ae5a6936def3ef2c6d8d04edcaf1ebbb83670
Kieran Mace <kieran@gpucluster.ucsf.edu>
2017-11-17 15:56:18 -0800

    code to generate genes with significant interaction information (without cdc15 or menadione)

---
+++ src/sci/lrt_genes_need_interaction.R
@@ -0,0 +1,16 @@
+load('intermediate/images/normalized_data.RData')
+library(DESeq2)
+dds = dds[,colData(dds)$Strain != 'CDC15']
+dds = dds[,colData(dds)$Condition != 'Menadione']
+colData(dds)$Strain = droplevels(colData(dds)$Strain)
+colData(dds)$Condition = droplevels(colData(dds)$Condition)
+m1 = model.matrix(~Strain*Condition, colData(dds))
+m2 = model.matrix(~Strain + Condition, colData(dds))
+
+all.zero <- apply(m1, 2, function(x) all(x==0))
+idx <- which(all.zero)
+m1 <- m1[,-idx]
+dds = DESeq(dds, test = 'LRT', full = m1, reduced = m2, betaPrior = FALSE)
+res= results(dds)
+save.image(file="ltr_test.RData")
+res %>% select(padj, name) %>% arrange(padj) %>% filter(padj<.05)
