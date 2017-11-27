2a1c501dc53ad69ae5ff5c6f53ff72015dbca715
Kieran Mace <kieran.mace@gmail.com>
2017-11-26 19:24:53 -0800

    code to generate modules from yeastnet

--- src/paper/figures/figure_4/get_modules.R
+++ src/paper/figures/figure_4/get_modules.R
@@ -1,24 +1,18 @@
-
-
 library(readr)
 library(igraph)
-
-yeastnet = read_delim('~/Downloads/YeastNet.v3.txt', delim = '\t', col_names = c('Gene_1', 'Gene_2', 'weight'))
-yeastnet %>% na.omit() %>% graph.data.frame(directed = FALSE) -> g
-
-adj = as_adjacency_matrix(g, attr='weight', type='both')
-
 library(gtools)
+library(WGCNA)
+library(dplyr)
 
+yeastnet = read_delim('../../../../input/external_datasets/YeastNet.v3.txt', delim = '\t', col_names = c('Gene_1', 'Gene_2', 'weight'))
+yeastnet %>% na.omit() %>% graph.data.frame(directed = FALSE) -> g
+adj = as_adjacency_matrix(g, attr='weight', type='both')
 adj = as.matrix(adj)
-
 adj_inv_logit = inv.logit(adj)
-
 adj_inv_logit = (adj_inv_logit*2 - 1)
 diag(adj_inv_logit) = 1
-
-pickSoftThreshold.fromSimilarity(similarity = adj_inv_logit)
-TOM = TOMsimilarity(adjMat = adj_inv_logit^4)
+p_est = pickSoftThreshold.fromSimilarity(similarity = adj_inv_logit)$powerEstimate
+TOM = TOMsimilarity(adjMat = adj_inv_logit^p_est)
 dissTOM = 1-TOM
 # Call the hierarchical clustering function
 geneTree = hclust(as.dist(dissTOM), method = "average");
@@ -33,4 +27,3 @@ minClusterSize = minModuleSize);
 
 m = data.frame(mod = dynamicMods, Gene = rownames(adj_inv_logit))
 m = left_join(m, t2g)
-
