9d9c81eff0c5ecfc7e3abc0c52e03c585cef9335
Kieran Mace <kieran.mace@gmail.com>
2018-05-12 23:01:17 -0700

    Remove duplicate files.

--- src/utils/meme/run_meme.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-query_file=$1
-file_base=`basename $1 .txt`
-query_genes=`cat $query_file | perl -pe 'chomp if eof' | tr '\n' '|'`
-cat all_sc_promoters.fasta | paste -d ':' - - | grep -E $query_genes | tr ':' '\n' > ${file_base}.fasta
-../meme/bin/ame --verbose 1 --oc ./${file_base} --control all_sc_promoters.fasta --bgformat 1 --scoring avg --method ranksum --pvalue-report-threshold 0.05 ${file_base}.fasta JASPAR_CORE_2016_fungi.meme
